53f979421f52e7a2843049aa8dd30041f0e26628
Erik Bussek <erikbussek@Eriks-MBP.local>
2021-01-05 15:42:25 +0100

    sending all services to the socket clients, if a service gets created, updated or deleted

--- Backend/src/api/service/controller.js
+++ Backend/src/api/service/controller.js
@@ -6,7 +6,10 @@ import { Service } from '.'
 export const create = ({ provider, bodymen: { body } }, res, next) =>
   Service.create({ ...body, provider })
     .then((service) => service.view(true))
-    .then(success(res, 201))
+    .then(function(){
+      success(res, 201);
+      sendServicesToSocketClients(query, select, cursor);
+    })
     .catch(next)
 
 export const index = ({ querymen: { query, select, cursor } }, res, next) =>
@@ -14,15 +17,8 @@ export const index = ({ querymen: { query, select, cursor } }, res, next) =>
     .populate('provider')
     .then((services) => services.map((service) => service.view()))
     .then(success(res))
-    .then(sendServicesToSocketClients("services"))
+   // .then(sendServicesToSocketClients(query, select, cursor))
     .catch(next)
-
-
-function sendServicesToSocketClients(services){
-  for (const socket of sockets) {
-    socket.emit('data', {bla:"blubb"});
-  }
-}
 
 export const show = ({ params }, res, next) =>
   Service.findById(params.id)
@@ -38,15 +34,35 @@ export const update = ({ bodymen: { body }, params }, res, next) =>
     .then(notFound(res))
     .then((service) => service ? Object.assign(service, body).save() : null)
     .then((service) => service ? service.view(true) : null)
-    .then(success(res))
+    .then(function(){
+      success(res);
+      sendServicesToSocketClients(query, select, cursor)
+    })
+    .then(sendServicesToSocketClients(query, select, cursor))
     .catch(next)
 
 export const destroy = ({ params }, res, next) =>
   Service.findById(params.id)
     .then(notFound(res))
     .then((service) => service ? service.remove() : null)
-    .then(success(res, 204))
+    .then(function(){
+      success(res, 204);
+      sendServicesToSocketClients(query, select, cursor)
+    })
     .catch(next)
 
+//send all services to the socket client if a new service was posted, a service gets updated or deleted
+function sendServicesToSocketClients(query, select, cursor){
+
+  for (const socket of sockets) {
+    //TODO query und co aus dem socket entnehmen, kann für jeden client anders sein
+    Service.find(query, select, cursor)
+      .populate('provider')
+      .then((services) => services.map((service) => socket.emit('data', service.view())))
+    //TODO welches error ahndling in dem fall ?!
+    // .catch(next)
+
+  }
+}
 
 
