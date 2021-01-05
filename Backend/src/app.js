3f1340c49f62e7beea2bf434e19e97d1b46fc454
Erik Bussek <erikbussek@Eriks-MBP.local>
2021-01-05 10:58:49 +0100

    import socket server in app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,9 +5,18 @@ import express from './services/express'
 import api from './api'
 var demoData = require('../demodaten/generateDemoData');
 
-
 const app = express(apiRoot, api)
 const server = http.createServer(app)
+
+const io = require('socket.io')(server.server);
+let sockets = new Set();
+
+io.on('connection', socket => {
+  sockets.add(socket);
+  socket.emit('data', { data: "products" });
+  socket.on('clientData', data => console.log(data));
+  socket.on('disconnect', () => sockets.delete(socket));
+});
 
 if (mongo.uri) {
   mongoose.connect(mongo.uri)
@@ -20,7 +29,7 @@ setImmediate(() => {
   server.listen(port, ip, () => {
     console.log('Express server listening on http://%s:%d, in %s mode', ip, port, env)
 
-    demoData.start();
+    //demoData.start();
   })
 })
 
