e0c14a1c5fc1deab9db81cff916d4d414563edd6
Tim <eberhardttimmarten@gmail.com>
2021-01-09 21:03:46 +0100

    First experiment with push notifications

--- Frontend/dondus/src/app/app.component.ts
+++ Frontend/dondus/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import webpush from 'web-push';
+import {SwPush, SwUpdate} from '@angular/service-worker';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'dondus';
+  constructor(
+    private update: SwUpdate,
+    private swPush: SwPush
+  ) {
+  }
+
+  // tslint:disable-next-line:typedef
+  //ngOnInit(){
+  //  this.subscribeToNotifications();
+  //}
+
+  // tslint:disable-next-line:typedef
+  //subscribeToNotifications() {
+  //  this.swPush.requestSubscription({
+  //    serverPublicKey: this.VAPID_PUBLIC_KEY
+  //  }).then(sub => console.log(sub)).catch(err => console.error('Could not subscribe to notifications', err));
+  //}
 }
+
+// console.log(webpush.generateVAPIDKeys());
+
+const publicKey = 'BJ5p-hXXpFo9SNXGBAeARqHUDiEecM7xEG1QoOTzrz6sUVd-wQuoklgqxI39MmiqMtsVbOCph-Ru3OPBdITUrW0';
+const privateKey = 'BsFfOsk1RrWzFoQvcp183stM2ybTuXduPIwt3RSyoFk';
--- Frontend/dondus/src/app/app.module.ts
+++ Frontend/dondus/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule, SwUpdate, SwPush } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,36 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSnackBarModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(update: SwUpdate, push: SwPush, snackbar: MatSnackBar) {
+    update.available.subscribe(aUpdate => {
+      console.log('update available');
+      const snack = snackbar.open('Hello World', 'Reload');
+
+      snack
+        .onAction()
+        .subscribe(() => {
+          window.location.reload();
+        });
+    });
+
+    push.messages.subscribe(msg => {
+      console.log(msg);
+      snackbar.open(JSON.stringify(msg));
+    });
+
+    const key = 'BJ5p-hXXpFo9SNXGBAeARqHUDiEecM7xEG1QoOTzrz6sUVd-wQuoklgqxI39MmiqMtsVbOCph-Ru3OPBdITUrW0';
+    push.requestSubscription({serverPublicKey: key})
+      .then(pushSubscription => {
+        console.log(pushSubscription.toJSON());
+      });
+  }
+}
