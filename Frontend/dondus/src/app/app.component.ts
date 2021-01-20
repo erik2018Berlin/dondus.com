43a7cd75d63fe6a5b57bfa46dd44f3504651eee5
Erik Bussek <erikbussek@Eriks-MBP.local>
2021-01-20 18:02:17 +0100

    first working navBar

--- Frontend/dondus/src/app/app.component.ts
+++ Frontend/dondus/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { CalendarOptions } from '@fullcalendar/angular';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +6,5 @@ import { CalendarOptions } from '@fullcalendar/angular';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dondus';
-
-  calendarOptions: CalendarOptions = {
-    initialView: 'dayGridMonth'
-  };
+  isCollapsed = true;
 }
--- Frontend/dondus/src/app/app.module.ts
+++ Frontend/dondus/src/app/app.module.ts
@@ -7,6 +7,18 @@ import { AppComponent } from './app.component';
 import { FullCalendarModule } from '@fullcalendar/angular';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import { IconsProviderModule } from './icons-provider.module';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { de_DE } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import de from '@angular/common/locales/de';
+
+registerLocaleData(de);
 
 FullCalendarModule.registerPlugins([
   dayGridPlugin,
@@ -20,9 +32,15 @@ FullCalendarModule.registerPlugins([
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FullCalendarModule
+    FullCalendarModule,
+    IconsProviderModule,
+    NzLayoutModule,
+    NzMenuModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: de_DE }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
