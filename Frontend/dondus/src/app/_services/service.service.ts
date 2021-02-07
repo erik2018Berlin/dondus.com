14b2e9144c8cd6242c46bb79c09392b0024136bd
Erik Bussek <erikbussek@Eriks-MBP.local>
2021-02-07 13:30:53 +0100

    booking lite is finished

--- Frontend/dondus/src/app/_services/service.service.ts
+++ Frontend/dondus/src/app/_services/service.service.ts
@@ -27,26 +27,31 @@ export class ServiceService {
 
 
   async setBooking(serviceId, date, currentUser) {
-/*
-    //POST Meeting-slot
-    const body = {access_token: currentUser.token, serviceId : serviceId   ,  date: date};
-    const meetingslot =  await this.http.post<any>(`http://127.0.0.1:9000/meeting-slots`, body).toPromise();
-    console.log("meetingslot: " + meetingslot);
+    try{
+      //POST Meeting-slot
+      const body = {access_token: currentUser.token, serviceId : serviceId   ,  date: date};
+      const meetingslot =  await this.http.post<any>(`http://127.0.0.1:9000/meeting-slots`, body).toPromise();
+      console.log("meetingslot: " + meetingslot);
 
-    //GET calendar from user
-    const params = new HttpParams().set('access_token', currentUser.token).set('userId', currentUser.user.id);
-    const calendarFromUser = await this.http.get<any>(`http://127.0.0.1:9000/calendars`, {params }).toPromise();
-    var meetingIds = calendarFromUser[0].meetingIds;
-    meetingIds.push(meetingslot.id);
-    console.log("calendar from User: " + calendarFromUser);
+      //GET calendar from user
+      const params = new HttpParams().set('access_token', currentUser.token).set('userId', currentUser.user.id);
+      const calendarFromUser = await this.http.get<any>(`http://127.0.0.1:9000/calendars`, {params }).toPromise();
+      var meetingIds = calendarFromUser[0].meetingIds;
+      meetingIds.push(meetingslot.id);
+      console.log("calendar from User: " + calendarFromUser);
 
 
-    //PUT calendar with id
-    const cal_body = {access_token: currentUser.token,meetingIds: meetingIds, name: calendarFromUser[0].name , userId: currentUser.user.id};
-    const updatedCalendar =  await this.http.put(`http://127.0.0.1:9000/calendars/` + calendarFromUser[0].id, cal_body).toPromise();
-    console.log("updated Calendar: " + updatedCalendar);
-    */
-    return true;
+      //PUT calendar with id
+      const cal_body = {access_token: currentUser.token,meetingIds: meetingIds, name: calendarFromUser[0].name , userId: currentUser.user.id};
+      const updatedCalendar =  await this.http.put(`http://127.0.0.1:9000/calendars/` + calendarFromUser[0].id, cal_body).toPromise();
+      console.log("updated Calendar: " + updatedCalendar);
+
+      return true;
+    }catch(e){
+      console.log(e);
+      return false;
+    }
+
 
   }
 
